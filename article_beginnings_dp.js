748d744ace91f5fa24e1f442219306c44e6519cb
Maksymilian Graczyk <maksgraczyk@users.noreply.github.com>
2018-07-04 14:09:19 +0200

    Update article_beginnings_dp.js

--- article_beginnings_dp.js
+++ article_beginnings_dp.js
@@ -28,7 +28,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-
 function addArticleBeginning(articleBlock, beginningText)
 {
     var paragraph = document.createElement("p");
