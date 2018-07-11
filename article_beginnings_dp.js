b2d8b93cfd70026ebc2cea5876c5f7e86029fe97
Maksymilian Graczyk <maksgraczyk@users.noreply.github.com>
2018-07-11 12:41:25 +0200

    Prepare the script for uploading to OpenUserJS

--- article_beginnings_dp.js
+++ article_beginnings_dp.js
@@ -4,7 +4,9 @@
 // @description    This script downloads and shows the beginning of all articles linked to from the main page or the publications list at dobreprogramy.pl.
 // @description:pl Ten skrypt pobiera i pokazuje początki wszystkich artykułów zalinkowanych ze strony głównej lub listy publikacji na stronie dobreprogramy.pl.
 // @version        1
+// @license        GPL-3.0
 // @grant          none
+// @updateURL      https://openuserjs.org/meta/super_max2/article_beginnings_dp.meta.js
 // @include        https://dobreprogramy.pl/
 // @include        https://www.dobreprogramy.pl/
 // @include        https://www.dobreprogramy.pl/Aktualnosci,*
